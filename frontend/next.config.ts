665ee55104ab074df62091fe9061e7588282cb51
jithendra259 <jithendrasubramanyam218@gmail.com>
2026-01-03 11:47:37 +0530

    navbar added

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        port: '',
+        pathname: '/**',  // Allows all paths under the hostname
+      },
+    ],
+  },
   /* config options here */
 };
 
