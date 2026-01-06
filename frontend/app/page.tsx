0e26b87f0b8f7c0dfdfb49cc6adb31b48a44e28b
jithendra259 <jithendrasubramanyam218@gmail.com>
2026-01-06 12:42:21 +0530

    project card added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,7 @@ import Chatbot from "./chatbot";
 import { HoveredLink, Menu,MenuItem, ProductItem  } from "@/components/ui/navbar-menu";
 import {cn} from "@/lib/utils";
 import { ProfileCard } from "@/components/profile-card";
+import { ProjectCard } from "@/components/ui/project-card";
 
 
 function Navbar({ className }: { className?: string }) {
@@ -64,6 +65,34 @@ function Navbar({ className }: { className?: string }) {
 }
 
 
+function ProjectCards({className}: {className?: string}) {
+  
+  return(
+     <div className="grid grid-cols-1 gap-8 p-8 md:grid-cols-2 lg:grid-cols-3">
+      <ProjectCard
+        title="Aero Landing Page"
+        description="A comprehensive AI chatbot platform. This project focuses on the design and development of a user-friendly and visually appealing landing page."
+        imgSrc="https://framerusercontent.com/images/hynA7mpUiyBRDcssvVKCDBT14IM.jpg"
+        link="#"
+      />
+      <ProjectCard
+        title="Dreamland App Concept"
+        description="A dreamy mobile app prototype designed for mindfulness and relaxation, featuring calming animations and a serene user interface."
+        imgSrc="https://framerusercontent.com/images/D4M3JTkvSAJaqyRe9AzUnHvL8Ao.jpg"
+        link="#"
+        linkText="Explore Concept"
+      />
+       <ProjectCard
+        title="Quantum Analytics Dashboard"
+        description="A data visualization tool for quantum computing experiments, providing real-time insights and complex data analysis."
+        imgSrc="https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070&auto=format&fit=crop"
+        link="#"
+      />
+    </div>
+  );
+
+}
+
 
 export default function Home() {
   return (
@@ -75,6 +104,7 @@ export default function Home() {
           </div>
           <div>
             <ProfileCard />
+            <ProjectCards />
             <Chatbot/>
           </div>
         </div>
